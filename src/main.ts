183a2844056b7343288a880b7a1fbd332891e632
pedrozau <pedrozau28@gmail.com>
2024-02-02 11:22:43 +0100

    adding  swagger doc api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+  const config = new DocumentBuilder()
+  .setTitle('Protaskapi')
+  .setDescription('The Protaskapi API description')
+  .setVersion('1.0')
+  .addTag('Protaskapi')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(3000);
 }

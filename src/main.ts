153444a6609c66180ddebca20a5ac037d49c9085
pedrozau <pedrozau28@gmail.com>
2024-02-02 13:33:24 +0100

    api docs changes

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   app.enableCors()
   const config = new DocumentBuilder().addBearerAuth()
   .setTitle('Protaskapi')
-  .setDescription('The Protaskapi API description')
+  .setDescription(`
+  The Protaskapi API description: 
+  Protask is a dynamic and engaging task management system designed to turn your daily to-dos into a rewarding and motivational experience. Unlike traditional task management tools, TaskRewards adds a layer of gamification, allowing users to not only accomplish their tasks but also earn and redeem exciting rewards within the 
+  `)
   .setVersion('1.0')
   .addTag('Protaskapi')
   .build();

918e2dfde563d2281fc782565d2841076f64dc9d
pedrozau <pedrozau28@gmail.com>
2024-02-02 10:45:38 +0100

    removed the csrf

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Base image
-FROM node:18
+FROM node:20.11.0
 
 # Create app directory
 WORKDIR /usr/src/app
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  app.use(csurf())
+  
   await app.listen(3000);
 }
 bootstrap();
